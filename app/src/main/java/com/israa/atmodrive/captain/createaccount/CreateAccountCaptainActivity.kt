25b024255c83c7695cb07595eb75593b0245d6fa
Israa Mohamed <esraamoh1072000@gmail.com>
2023-07-28 12:06:12 +0300

    Initial commit

---
+++ app/src/main/java/com/israa/atmodrive/captain/createaccount/CreateAccountCaptainActivity.kt
@@ -0,0 +1,12 @@
+package com.israa.atmodrive.captain.createaccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.israa.atmodrive.R
+
+class CreateAccountCaptainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_captain)
+    }
+}
---
+++ app/src/main/java/com/israa/atmodrive/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.israa.atmodrive.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.israa.atmodrive.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
